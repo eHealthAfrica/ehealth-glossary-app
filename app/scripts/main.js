ffc58bdf497748b2abd953a1175abdca7a716a17
Alex Feyerke <espy@users.noreply.github.com>
2014-10-28 14:45:19 +0100

    feat: highlighting of the searc term

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,7 @@
 
 var glossaryApp = angular.module('glossaryApp', ['ngStorage', function () {
   // If we're not in our dev environment, start appCache
-  if(window.location.port !== "9000"){
+  if(window.location.port !== '9000'){
     appCacheNanny.start();
   }
 }]);
@@ -54,6 +54,37 @@ glossaryApp.filter('rawHtml', ['$sce', function($sce){
     return $sce.trustAsHtml(val);
   };
 }]);
+
+glossaryApp.filter('highlight', function($sce) {
+  return function(text, phrase) {
+    // Find all a tags
+    var links = text.match(/<a.*(?=<\/a>)<\/a>/, 'gi');
+    // Replace each a tag with a token
+    if(links){
+      links.forEach(function(link, index){
+        text = text.replace(new RegExp('('+link+')', 'i'), '§§'+index);
+      });
+    }
+    // highlight the rest of the text
+    if (phrase) {
+      text = text.replace(new RegExp('('+phrase+')', 'gi'),
+        '<span class="highlighted">$1</span>');
+    }
+    if(links){
+      links.forEach(function(link, index){
+        // if the link contains the query phrase, wrap it in a highlight
+        // and swap with its token
+        if(phrase !== '' && link.match(new RegExp('('+phrase+')', 'gi'))){
+          text = text.replace('§§'+index, '<span class="highlighted">'+link+'</span>');
+        } else {
+          // If the link doesn't contain the query, just swap it with its token
+          text = text.replace('§§'+index, link);
+        }
+      });
+    }
+    return $sce.trustAsHtml(text);
+  };
+});
 
 glossaryApp.filter('linkify', function(){
   return function (inputText) {
